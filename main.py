e0f1964688ef04f4e57ed4e824904d32f2ee5fb2
gargi-0912 <gargigupta09122003@gmail.com>
2025-06-21 12:26:25 +0530

    path parameters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app=FastAPI()
 # We can replace app with any other name also
@@ -13,3 +14,33 @@ async def post():
 @app.put("/")
 async def put():
     return {"message":"hello from the put route"}
+
+@app.get("/users")
+async def list_items():
+    return {"message":"list users route"}
+
+@app.get("/user/me")
+async def get_current_user():
+    return {"Message":"this is the current user"}
+
+@app.get("/users/{user_id}")
+async def get_user(user_id:str):
+    return {"user_id":user_id}
+
+class FoodEnum(str,Enum):
+    fruits="fruits"
+    vegetables="vegetables"
+    dairy="dairy"
+
+@app.get("/foods/{food_name}")
+async def get_food(food_name:FoodEnum):
+    if food_name==FoodEnum.vegetables:
+        return {"food_name":food_name,"message":"you are healthy"}
+    
+    if food_name.value=='fruits':
+        return {
+                "food_name":food_name,
+                "message":"you are still healthy but like "
+                "sweet thing"
+                }
+    return {"food_name":food_name,"message":"i like chocolate"}
